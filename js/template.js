9f9331c3f8066d9ef675a5641790c24d37b96280
myfreepic123 <111050459+myfreepic123@users.noreply.github.com>
2022-08-11 15:30:45 +0530

    muthu
    new

---
+++ js/template.js
@@ -0,0 +1,18 @@
+//[Master Javascript]
+
+//Project:	Shopping - Onepage Html Responsive Template
+//Version:	1.1
+//Last change:	09/05/2017
+//Primary use:	Shopping - Onepage Html Responsive Template
+
+
+//Template script here
+
+$(document).ready(function(){
+    
+	// prettyPhoto
+
+	$("a[rel^='alternate']").prettyPhoto();
+	
+   
+}); // End of use strict
